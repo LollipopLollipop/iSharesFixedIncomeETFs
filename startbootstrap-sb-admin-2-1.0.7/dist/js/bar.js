aae1b70320e668d1679c42cc4026a14f8ce07ff2
LollipopLollipop <jodie.d.zhao@gmail.com>
2015-07-21 09:02:47 -0700

    add actual content

---
+++ startbootstrap-sb-admin-2-1.0.7/dist/js/bar.js
@@ -0,0 +1,22 @@
+var chart = c3.generate({
+    bindto: '#bar-chart',
+	size: {
+        height: 360,
+        width: 360
+    },
+    data: {
+        columns: [
+            ['data1', 30, 200, 100, 400, 150, 250],
+            ['data2', 130, 100, 140, 200, 150, 50]
+        ],
+        type: 'bar'
+    },
+    bar: {
+        width: {
+            ratio: 0.5 // this makes bar width 50% of length between ticks
+        }
+        // or
+        //width: 100 // this makes bar width 100px
+    }
+});
+
